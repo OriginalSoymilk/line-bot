7f32f9e40516e9c03ffb8d86663cbb3ad67b6152
OriginalSoymilk <126840360+OriginalSoymilk@users.noreply.github.com>
2023-04-18 13:19:40 +0800

    Update line.py

--- line.py
+++ line.py
@@ -33,7 +33,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text
-    reply_text = 'Hello, you said: ' + text
+    reply_text =  text
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply_text)
